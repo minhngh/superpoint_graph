2e4ea29eedec989aef741deae59c800623fd8a65
minhngh <minh.nghminh@gmail.com>
2021-08-06 18:18:35 +0700

    add PE for gated-gcn

--- gated_gcn/gated_gcn_model.py
+++ gated_gcn/gated_gcn_model.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn as nn
+import numpy as np
+from scipy import sparse as sp
 
 from .gated_gcn_layer import GatedGCNLayer
 
@@ -34,16 +36,32 @@ class GatedGCNNet(nn.Module):
         self.residual = params.residual
         self.n_classes = n_classes
         self.device = torch.device('cuda' if params.cuda else 'cpu')
+        self.pos_enc_dim = params.pos_enc_dim
         
         self.embedding_e = nn.Linear(in_dim_edge, in_dim_node) # edge feat is a float
+        if params.pos_enc_dim:
+            self.embedding_pos_enc = nn.Linear(self.pos_enc_dim, in_dim_node)
         self.layers = nn.ModuleList([ GatedGCNLayer(hidden_dim if i > 0 else in_dim_node, hidden_dim, dropout,
                                                     self.batch_norm, self.residual) for i in range(n_layers) ])
         self.MLP_layer = MLPReadout(hidden_dim, n_classes)
-        
+    
+    def positional_encoding(self, g, pos_enc_dim):
+        n = g.num_nodes()
+        A = g.adj(scipy_fmt = 'coo')
+        N = sp.diags(g.in_degrees().clip(min = 1).numpy() ** -0.5, dtype = float)
+        L = sp.eye(n) - N.dot(A).dot(N)
+        eig_value, eig_vector = np.linalg.eig(L.toarray())
+        idx = eig_value.argsort()
+        eig_value =eig_value[idx]
+        eig_vector = np.real(eig_vector[:, idx])
+        g.ndata['pos_enc'] = torch.Tensor(eig_vector[:, 1 : pos_enc_dim + 1])
 
     def forward(self, g, h):
+        self.positional_encoding(g, self.pos_enc_dim)
         e = g.edata['f']
         e = self.embedding_e(e)
+        if self.pos_enc_dim:
+            h = h + self.embedding_pos_enc(g.ndata['pos_enc'])
         
         # res gated convnets
         for conv in self.layers:
